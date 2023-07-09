947044896ab53689a0e76796e9392f1e47ef6031
Hamidov Siyovush <93854666+siyovushchik1414@users.noreply.github.com>
2023-07-09 12:16:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ st.set_page_config(page_title='Hamidov Siyovush', page_icon=None, initial_sideba
 st.title("  Hamido1 Siyovush's")
 st.title("Certificates and Courses")
 
-pdf_url = "https://drive.google.com/uc?id=11AnC1jZwve4YsvSbr9-mS-v6q5ZmpoAm"
-pdf_viewer_url = f"http://docs.google.com/gview?url={pdf_url}&embedded=true"
+file_id = '11AnC1jZwve4YsvSbr9-mS-v6q5ZmpoAm'
+file_url = f'https://drive.google.com/file/d/{file_id}/view'
+
+st.markdown(f'**PDF:** [link]({file_url})')
 
 st.components.v1.iframe(pdf_viewer_url, width=800, height=1000)
