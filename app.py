f6fe8cfe3aa03dcc99126f2ab87e8253d98ba20b
Hamidov Siyovush <93854666+siyovushchik1414@users.noreply.github.com>
2023-07-09 10:50:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
   st.set_page_config(page_title='Hamidov Siyovush', page_icon=None, initial_sidebar_state='auto')
-  st.title("Hamidov Siyovush's")
+  st.title("  Hamidov Siyovush's")
   st.title("Certificates and Courses")
 
 if __name__ == "__main__":
