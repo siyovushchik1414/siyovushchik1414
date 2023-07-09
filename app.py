db8e49d1cf31e940fb1a2779f821fda983e16c65
Hamidov Siyovush <93854666+siyovushchik1414@users.noreply.github.com>
2023-07-09 11:57:25 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,6 @@ st.title("  Hamido1 Siyovush's")
 st.title("Certificates and Courses")
 
 pdf_url = "https://drive.google.com/uc?id=11AnC1jZwve4YsvSbr9-mS-v6q5ZmpoAm"
+pdf_viewer_url = f"http://docs.google.com/gview?url={pdf_url}&embedded=true"
 
-# Download the PDF file from Google Drive
-response = requests.get(pdf_url)
-pdf_content = response.content
-
-# Encode the PDF content using base64 and display it using st.markdown
-base64_pdf = base64.b64encode(pdf_content).decode('utf-8')
-pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-st.markdown(pdf_display, unsafe_allow_html=True)
+st.components.v1.iframe(pdf_viewer_url, width=800, height=1000)
