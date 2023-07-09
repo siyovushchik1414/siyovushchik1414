4de112a6dcac1c9d89416e72d15bd1d557dd5510
Hamidov Siyovush <93854666+siyovushchik1414@users.noreply.github.com>
2023-07-09 12:24:44 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ st.set_page_config(page_title='Hamidov Siyovush', page_icon=None, initial_sideba
 st.title("  Hamido1 Siyovush's")
 st.title("Certificates and Courses")
 
-file_id = '11AnC1jZwve4YsvSbr9-mS-v6q5ZmpoAm'
-file_url = f'https://drive.google.com/file/d/{file_id}/view'
+ExpData = '11AnC1jZwve4YsvSbr9-mS-v6q5ZmpoAm'
+FundScal = '1hzgd6LaYpzyCELeQ9kTL4F4uy3du7JYP'
+ToolsForDS = '13a2T84g1JNiKfEMmvKs9Z7BFcEJK2VEB'
+Django = '103zM5cLiqrxm4P8GQ1saP9a9rUQIuN5q'
 
-st.markdown(f'**PDF:** [link]({file_url})')
+ExpDataURL = f'https://drive.google.com/file/d/{ExpData}/view'
+FundScalURL = f'https://drive.google.com/file/d/{FundScal}/view'
+ToolsForDSURL = f'https://drive.google.com/file/d/{ToolsForDS}/view'
+DjangoURL = f'https://drive.google.com/file/d/{Django}/view'
+
+st.markdown(f'**PDF:** [Exploratory Data Analysis for Machine Learning]({ExpDataURL})')
+st.markdows(f'**PDF:** [https://www.coursera.org/account/accomplishments/verify/B32838V98B53])
