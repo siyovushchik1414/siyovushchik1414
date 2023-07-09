64e460c3b1a9b2854f67ac2f4dd8355d1d1f82f7
Hamidov Siyovush <93854666+siyovushchik1414@users.noreply.github.com>
2023-07-09 12:34:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -6,21 +6,10 @@ st.set_page_config(page_title='Hamidov Siyovush', page_icon=None, initial_sideba
 st.title("  Hamido1 Siyovush's")
 st.title("Certificates and Courses")
 
-ExpData = '11AnC1jZwve4YsvSbr9-mS-v6q5ZmpoAm'
-FundScal = '1hzgd6LaYpzyCELeQ9kTL4F4uy3du7JYP'
-ToolsForDS = '13a2T84g1JNiKfEMmvKs9Z7BFcEJK2VEB'
-Django = '103zM5cLiqrxm4P8GQ1saP9a9rUQIuN5q'
+st.markdown(f'**Website:** [Exploratory Data Analysis for Machine Learning]({https://www.coursera.org/account/accomplishments/verify/B32838V98B53})')
 
-ExpDataURL = f'https://drive.google.com/file/d/{ExpData}/view'
-FundScalURL = f'https://drive.google.com/file/d/{FundScal}/view'
-ToolsForDSURL = f'https://drive.google.com/file/d/{ToolsForDS}/view'
-DjangoURL = f'https://drive.google.com/file/d/{Django}/view'
-
-st.markdown(f'**PDF:** [Exploratory Data Analysis for Machine Learning]({ExpDataURL})')
-st.markdows('https://www.coursera.org/account/accomplishments/verify/B32838V98B53]')
 
 from pdf2image import convert_from_path
-import streamlit as st
 
 # Замените 'path/to/file.pdf' на соответствующее значение
 images = convert_from_path('Exploratory Data Analysis for Machine Learning.pdf')
